222e7675be490a920ad97fcbf807fd0a528f5ead
Ram <im_ram@hotmail.com>
2022-08-04 09:56:03 +1000

    added domain join class

---
+++ site-modules/profile/manifests/domain_membership.pp
@@ -0,0 +1,36 @@
+class domain_membership (
+  $domain = 'W2K16DC01.HomeLab.local',
+  $username = 'administrator',
+  $password = 'K@thmand0Ra',
+  $secure_password = false,
+  $machine_ou      = 'OU=VMs,DC=HomeLab,DC=local',
+){
+
+$code = " \
+\$secStr=ConvertTo-SecureString '${password}' -AsPlainText -Force; \
+if (-not \$?) { \
+write-error 'Error: Unable to convert password string to a secure string'; \
+exit 10; \
+} \
+\$creds=New-Object System.Management.Automation.PSCredential( '${username}', \$secStr ); \
+if (-not \$?) { \
+write-error 'Error: Unable to create PSCredential object'; \
+exit 20; \
+} \
+Add-Computer -DomainName ${domain} -OUPath $_machine_ou -Restart -Force -Cred \$creds; \
+if (-not \$?) { \
+write-error 'Error: Unable to join domain'; \
+exit 30; \
+} \
+exit 0"
+
+#
+# Use the Josh Cooper PowerShell provider
+#
+exec { 'join_domain':
+command => $code,
+provider => powershell,
+logoutput => true,
+unless => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne '${domain}') { exit 1 }",
+}
+}
--- site-modules/profile/manifests/soe.pp
+++ site-modules/profile/manifests/soe.pp
@@ -5,6 +5,7 @@ $soe_os =$facts['os']['name']
 case $soe_os{
   'windows':{
    include profile::windows
+   include profile::domain_membership
   }
   'CentOS','Redhat':{
     include profile::linux
