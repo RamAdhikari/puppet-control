747b134686be40c0638feb57be7ae191458fd8da
RamAdhikari <32997524+RamAdhikari@users.noreply.github.com>
2022-08-04 17:58:14 +1000

    Update domain_membership.pp

--- site-modules/profile/manifests/domain_membership.pp
+++ site-modules/profile/manifests/domain_membership.pp
@@ -1,18 +1,7 @@
 class profile::domain_membership {
-
-$code = " 
- $domain = 'HomeLab.local'
- $machine_ou = 'OU=VMs,DC=HomeLab,DC=local'
- $username='homelab\administrator'
- $password = ConvertTo-SecureString 'K@thmand0Ra' -AsPlainText -Force
- $cred=New-Object System.Management.Automation.PSCredential ('$username','$password')
- Add-Computer -DomainName '$domain' -OUPath '$machine_ou'  -Restart -Force -Credential '$cred'
-"
-
 exec { 'join_domain':
-command => $code,
+command => file('profile/dj.ps1'),
 provider => powershell,
-logoutput => true,
-unless => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne '${domain}') { exit 1 }",
+unless => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne 'homelab.local') { exit 1 }",
 }
 }
