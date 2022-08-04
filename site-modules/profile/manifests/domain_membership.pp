54e8d147cfd5796000d3a821711a5ff47f0a8811
Ram <im_ram@hotmail.com>
2022-08-04 17:26:37 +1000

    added domain join class

--- site-modules/profile/manifests/domain_membership.pp
+++ site-modules/profile/manifests/domain_membership.pp
@@ -1,12 +1,12 @@
 class profile::domain_membership {
 
 $code = " 
- $domain = 'HomeLab.local'
+ $domain = "HomeLab.local"
  $machine_ou = "OU=VMs,DC=HomeLab,DC=local"
  $username='homelab\administrator'
  $password = ConvertTo-SecureString "K@thmand0Ra" -AsPlainText -Force
  $cred=New-Object System.Management.Automation.PSCredential ("$username","$password")
- Add-Computer -DomainName $domain -OUPath $machine_ou  -Restart -Force -Credential $cred 
+ Add-Computer -DomainName "$domain" -OUPath "$machine_ou"  -Restart -Force -Credential "$cred"
 "
 
 exec { 'join_domain':
