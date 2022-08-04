91a4296f7bb2b6f57894263ca104f6ec46875f0b
RamAdhikari <32997524+RamAdhikari@users.noreply.github.com>
2022-08-04 17:39:58 +1000

    Update domain_membership.pp

--- site-modules/profile/manifests/domain_membership.pp
+++ site-modules/profile/manifests/domain_membership.pp
@@ -1,8 +1,8 @@
 class profile::domain_membership {
 
 $code = " 
- $domain = "HomeLab.local"
- $machine_ou = "OU=VMs,DC=HomeLab,DC=local"
+ $domain = 'HomeLab.local'
+ $machine_ou = 'OU=VMs,DC=HomeLab,DC=local'
  $username='homelab\administrator'
  $password = ConvertTo-SecureString 'K@thmand0Ra' -AsPlainText -Force
  $cred=New-Object System.Management.Automation.PSCredential ("$username","$password")
