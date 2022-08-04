d4de8e8e19082bf59a80f7801db8abc947f158d2
Ram <im_ram@hotmail.com>
2022-08-04 17:20:37 +1000

    added domain join class

--- site-modules/profile/manifests/domain_membership.pp
+++ site-modules/profile/manifests/domain_membership.pp
@@ -1,32 +1,14 @@
-class profile::domain_membership (
-  $domain = 'HomeLab.local',
-  $username = 'administrator',
-  $password = 'K@thmand0Ra',
-  $secure_password = false,
-  $machine_ou      = 'OU=VMs,DC=HomeLab,DC=local',
-){
+class profile::domain_membership {
 
-$code = " \
-\$secStr=ConvertTo-SecureString '${password}' -AsPlainText -Force; \
-if (-not \$?) { \
-write-error 'Error: Unable to convert password string to a secure string'; \
-exit 10; \
-} \
-\$creds=New-Object System.Management.Automation.PSCredential( '${username}', \$secStr ); \
-if (-not \$?) { \
-write-error 'Error: Unable to create PSCredential object'; \
-exit 20; \
-} \
-Add-Computer -DomainName ${domain}  -Restart -Force -Cred \$creds; \
-if (-not \$?) { \
-write-error 'Error: Unable to join domain'; \
-exit 30; \
-} \
-exit 0"
+$code = " 
+ $domain = 'HomeLab.local'
+ $machine_ou = 'OU=VMs,DC=HomeLab,DC=local'
+ $username='homelab\administrator'
+ $password = ConvertTo-SecureString "K@thmand0Ra" -AsPlainText -Force
+ $cred=New-Object System.Management.Automation.PSCredential ("$username","$password")
+ Add-Computer -DomainName $domain -OUPath $machine_ou  -Restart -Force -Credential $cred 
+"
 
-#
-# Use the Josh Cooper PowerShell provider
-#
 exec { 'join_domain':
 command => $code,
 provider => powershell,
