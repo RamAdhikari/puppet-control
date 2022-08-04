3fe6b4d21d6969a26c91bd6fbfb7706ad0d5338f
RamAdhikari <32997524+RamAdhikari@users.noreply.github.com>
2022-08-04 17:41:03 +1000

    Update domain_membership.pp

--- site-modules/profile/manifests/domain_membership.pp
+++ site-modules/profile/manifests/domain_membership.pp
@@ -5,7 +5,7 @@ $code = "
  $machine_ou = 'OU=VMs,DC=HomeLab,DC=local'
  $username='homelab\administrator'
  $password = ConvertTo-SecureString 'K@thmand0Ra' -AsPlainText -Force
- $cred=New-Object System.Management.Automation.PSCredential ("$username","$password")
+ $cred=New-Object System.Management.Automation.PSCredential ('$username','$password')
  Add-Computer -DomainName "$domain" -OUPath "$machine_ou"  -Restart -Force -Credential "$cred"
 "
 
